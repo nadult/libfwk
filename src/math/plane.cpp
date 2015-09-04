87787f58f33b8d6710091dbeff4227a49cebca3a
Krzysztof Jakubowski <nadult@mail.com>
2015-09-04 20:27:57 +0200

    Fixed plane matrix multiplication

--- src/math/plane.cpp
+++ src/math/plane.cpp
@@ -14,9 +14,25 @@ const Plane normalize(const Plane &plane) {
 }
 
 const Plane operator*(const Matrix4 &m, const Plane &p) {
-	float3 new_p = mulPoint(m, p.normal() * p.distance());
-	float3 new_n = normalize(mulNormal(m, p.normal()));
-	return Plane(new_n, dot(new_n, new_p));
+	float3 v1 = p.normal();
+	int min = v1[0] < v1[1] ? 0 : 1;
+	if(v1[2] < v1[min])
+		min = 2;
+	v1[min] = 2.0f;
+	v1 = normalize(v1);
+	float3 v2 = cross(p.normal(), v1);
+
+	float3 p0 = p.normal() * p.distance();
+	float3 p1 = p0 + v1, p2 = p0 + v2;
+	p1 -= p.normal() * dot(p, p1);
+	p2 -= p.normal() * dot(p, p2);
+
+	return Plane(m * Triangle(p0, p1, p2));
+
+	// TODO: this is incorrect:
+	/*	float3 new_p = mulPoint(m, p.normal() * p.distance());
+		float3 new_n = normalize(mulNormal(m, p.normal()));
+		return Plane(new_n, dot(new_n, new_p));*/
 }
 
 float dot(const Plane &plane, const float3 &point) {
