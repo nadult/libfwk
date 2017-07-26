70ad09c8cfd63b19b7482413fb24b0b89754feb2
Krzysztof Jakubowski <nadult@fastmail.fm>
2017-07-26 20:35:29 +0200

    Minor improvements in fwk flags

--- include/fwk_base.h
+++ include/fwk_base.h
@@ -570,12 +570,13 @@ using BestFlagsBase =
 	Conditional<count<T>() <= 8, u8,
 				Conditional<count<T>() <= 16, u16, Conditional<count<T>() <= 32, u32, u64>>>;
 
-template <class T, class Base = BestFlagsBase<T>> struct EnumFlags {
+template <class T, class Base_ = BestFlagsBase<T>> struct EnumFlags {
+	using Base = Base_;
 	enum { max_flags = fwk::count<T>() };
 	static constexpr const Base mask =
 		(Base(1) << (max_flags - 1)) - Base(1) + (Base(1) << (max_flags - 1));
 
-	static_assert(isEnum<T>(), "Flag<> should be based on fwk-enum");
+	static_assert(isEnum<T>(), "EnumFlags<> should be based on fwk-enum");
 	static_assert(std::is_unsigned<Base>::value, "");
 	static_assert(fwk::count<T>() <= sizeof(Base) * 8, "");
 
@@ -584,14 +585,14 @@ template <class T, class Base = BestFlagsBase<T>> struct EnumFlags {
 	constexpr explicit EnumFlags(Base bits) : bits(bits) {}
 	template <class TBase> constexpr EnumFlags(EnumFlags<T, TBase> rhs) : bits(rhs.bits) {}
 
-	constexpr EnumFlags operator|(const EnumFlags &rhs) const { return EnumFlags(bits | rhs.bits); }
-	constexpr EnumFlags operator&(const EnumFlags &rhs) const { return EnumFlags(bits & rhs.bits); }
-	constexpr EnumFlags operator^(const EnumFlags &rhs) const { return EnumFlags(bits ^ rhs.bits); }
+	constexpr EnumFlags operator|(EnumFlags rhs) const { return EnumFlags(bits | rhs.bits); }
+	constexpr EnumFlags operator&(EnumFlags rhs) const { return EnumFlags(bits & rhs.bits); }
+	constexpr EnumFlags operator^(EnumFlags rhs) const { return EnumFlags(bits ^ rhs.bits); }
 	constexpr EnumFlags operator~() const { return EnumFlags((~bits) & (mask)); }
 
-	constexpr void operator|=(const EnumFlags &rhs) { bits |= rhs.bits; }
-	constexpr void operator&=(const EnumFlags &rhs) { bits &= rhs.bits; }
-	constexpr void operator^=(const EnumFlags &rhs) { bits ^= rhs.bits; }
+	constexpr void operator|=(EnumFlags rhs) { bits |= rhs.bits; }
+	constexpr void operator&=(EnumFlags rhs) { bits &= rhs.bits; }
+	constexpr void operator^=(EnumFlags rhs) { bits ^= rhs.bits; }
 
 	constexpr bool operator==(T rhs) const { return bits == (Base(1) << int(rhs)); }
 	constexpr bool operator==(EnumFlags rhs) const { return bits == rhs.bits; }
@@ -639,7 +640,7 @@ template <class T> using EnableIfEnumFlags = EnableIf<detail::IsEnumFlags<T>::va
 template <class TFlags, EnableIfEnumFlags<TFlags>...> constexpr int countBits(const TFlags &flags) {
 	int out = 0;
 	for(int i = 0; i < TFlags::max_flags; i++)
-		out += flags.bits & (1 << i) ? 1 : 0;
+		out += flags.bits & (typename TFlags::Base(1) << i) ? 1 : 0;
 	return out;
 }
 
