df7bcbec6346b2620749cfd498a135abb6f114e6
Krzysztof Jakubowski <nadult@fastmail.fm>
2017-10-12 09:59:48 +0200

    Rollback-enabled PNG loader

--- src/gfx/texture_png.cpp
+++ src/gfx/texture_png.cpp
@@ -2,30 +2,36 @@
 // This file is part of libfwk. See license.txt for details.
 
 #include "fwk/gfx/texture.h"
-#include "fwk/sys/stream.h"
+
 #include <png.h>
+
+#include "fwk/sys/assert.h"
+#include "fwk/sys/rollback.h"
+#include "fwk/sys/stream.h"
 
 namespace fwk {
 namespace detail {
 
-	class PngLoader {
-	  public:
-		PngLoader(Stream &stream) : stream(stream), m_struct(nullptr), m_info(nullptr) {
+	struct PngLoader {
+		PngLoader(Stream &stream) : stream(stream) {
+			s_loader = this;
+
 			enum { sig_size = 8 };
 			unsigned char signature[sig_size];
 			stream.loadData(signature, sig_size);
 			stream.seek(stream.pos() - sig_size);
 			if(png_sig_cmp(signature, 0, sig_size))
-				FATAL("Wrong PNG file signature");
+				fail("Wrong file signature");
 
-			m_struct =
-				png_create_read_struct(PNG_LIBPNG_VER_STRING, 0, errorCallback, warningCallback);
+			m_struct = png_create_read_struct_2(PNG_LIBPNG_VER_STRING, 0, errorCallback,
+												warningCallback, nullptr, mallocFunc, freeFunc);
 			m_info = png_create_info_struct(m_struct);
 
-			if(!m_struct || !m_info) {
-				finalCleanup();
-				FATAL("Error while initializing PNGLoader");
-			}
+			if(!m_struct || !m_info)
+				fail("Error while initializing libpng");
+
+			if(setjmp(png_jmpbuf(m_struct)))
+				fail();
 
 			png_set_read_fn(m_struct, this, readCallback);
 			png_read_info(m_struct, m_info);
@@ -54,31 +60,46 @@ namespace detail {
 
 			m_width = width;
 			m_height = height;
+
+			if(!m_error.empty())
+				fail();
 		}
 
-		~PngLoader() { finalCleanup(); }
+		~PngLoader() {
+			cleanup();
+			s_loader = nullptr;
+		}
+
 		PngLoader(const PngLoader &) = delete;
 		void operator=(const PngLoader &) = delete;
 
 		int2 size() const { return {m_width, m_height}; }
 
-		void operator>>(vector<u16> &out) const {
+		void operator>>(vector<u16> &out) {
 			ASSERT(m_bit_depth == 16);
 			ASSERT(m_color_type == PNG_COLOR_TYPE_GRAY);
 
+			vector<u8 *> row_pointers(m_height);
+
+			if(setjmp(png_jmpbuf(m_struct)))
+				fail();
+
+				// TODO: why swap on mingw?
 #if defined(__LITTLE_ENDIAN__) || defined(FWK_TARGET_MINGW)
 			if(m_bit_depth == 16)
 				png_set_swap(m_struct);
 #endif
 
-			vector<u8 *> row_pointers(m_height);
 			out.resize(m_width * m_height);
 			for(int y = 0; y < m_height; y++)
 				row_pointers[y] = (u8 *)&out[m_width * y];
 			png_read_image(m_struct, &row_pointers[0]);
+
+			if(!m_error.empty())
+				return fail();
 		}
 
-		void operator>>(Span<IColor> out) const {
+		void operator>>(Span<IColor> out) {
 			DASSERT(out.size() >= m_width * m_height);
 			vector<u8> temp;
 			temp.resize(m_width * m_height * 4);
@@ -90,15 +111,18 @@ namespace detail {
 			png_colorp palette = 0;
 			int numPalette = 0;
 
+			if(setjmp(png_jmpbuf(m_struct)))
+				fail();
+
 			for(int y = 0; y < m_height; y++)
 				rowPointers[y] = &temp[m_width * 4 * y];
 
 			if(m_color_type == PNG_COLOR_TYPE_PALETTE) {
 				if(m_channels != 1)
-					FATAL("Only 8-bit palettes are supported");
+					return fail("Only 8-bit palettes are supported");
 
 				if(!png_get_PLTE(m_struct, m_info, &palette, &numPalette))
-					FATAL("Error while retrieving PNG palette");
+					return fail("Error while retrieving palette");
 
 				if(png_get_valid(m_struct, m_info, PNG_INFO_tRNS))
 					png_get_tRNS(m_struct, m_info, &trans, &numTrans, 0);
@@ -130,27 +154,53 @@ namespace detail {
 							IColor(src[x * 4 + 0], src[x * 4 + 1], src[x * 4 + 2], src[x * 4 + 3]);
 				}
 			}
+
+			if(!m_error.empty())
+				fail();
 		}
 
 	  private:
-		void finalCleanup() { png_destroy_read_struct(&m_struct, &m_info, 0); }
+		[[noreturn]] void fail(const char *text = "") {
+			cleanup();
+
+			string msg = text;
+			if(!m_error.empty())
+				msg += "\n" + toString(m_error);
+			CHECK_FAILED("PNG loading error: %s", msg.c_str());
+		}
+
+		void cleanup() {
+			if(m_struct) {
+				png_destroy_read_struct(&m_struct, m_info ? &m_info : nullptr, 0);
+				m_struct = nullptr;
+				m_info = nullptr;
+			}
+		}
 		static void readCallback(png_structp png_ptr, png_bytep data, png_size_t length) {
 			auto *loader = static_cast<PngLoader *>(png_get_io_ptr(png_ptr));
 			loader->stream.loadData(data, length);
 		}
 
-		static void errorCallback(png_structp png_ptr, png_const_charp message) {
-			FATAL("LibPNG Error: %s", message);
-		}
+		static void *mallocFunc(png_struct *, size_t count) { return fwk::allocate(count); }
+		static void freeFunc(png_struct *, void *ptr) { return fwk::deallocate(ptr); }
 
+		static void errorCallback(png_structp png_ptr, png_const_charp message) {
+			if(auto *loader = s_loader)
+				loader->m_error = Error({message});
+		}
 		static void warningCallback(png_structp png_ptr, png_const_charp message) {}
 
+		static __thread PngLoader *s_loader;
+
+		Error m_error;
 		Stream &stream;
-		png_structp m_struct;
-		png_infop m_info;
+		png_structp m_struct = nullptr;
+		png_infop m_info = nullptr;
 		int m_color_type, m_bit_depth, m_channels;
-		int m_width, m_height;
+		int m_width = 0, m_height = 0;
 	};
+
+	__thread PngLoader *PngLoader::s_loader = nullptr;
 
 	void loadPNG(Stream &stream, PodVector<IColor> &out_data, int2 &out_size) {
 		PngLoader loader(stream);
