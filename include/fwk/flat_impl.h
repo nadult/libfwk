a12f1fbe2bcb85274e8b4a5646f5dd4c1eabe0b9
Krzysztof Jakubowski <nadult@fastmail.fm>
2017-11-02 23:13:39 +0100

    FlatImpl properly handles partially specified types

--- include/fwk/flat_impl.h
+++ include/fwk/flat_impl.h
@@ -20,18 +20,32 @@ template <class T, int size, int align> struct alignas(align) FlatProxy {
 };
 
 namespace detail {
+	// Doesn't work for private members
+	template <class T> struct IsDefined {
+		template <class C> static auto test(int) -> IndexedType<C, sizeof(C)>;
+		template <class C> static auto test(...) -> void;
+		static constexpr bool value = !std::is_same<void, decltype(test<T>(0))>::value;
+	};
+
+	template <class T> struct ArgsDefined { static constexpr bool value = true; };
+	template <template <class...> class T, class... Args> struct ArgsDefined<T<Args...>> {
+		static constexpr bool value = Conjunction<IsDefined<Args>...>::value;
+	};
+
 	template <class T, int size, int align, int req_size, int req_align>
 	using ValidateFlatImpl = Conditional<
 		size != req_size, InvalidFlatImplSize<T, size, req_size>,
 		Conditional<align != req_align, InvalidFlatImplAlignment<T, align, req_align>, T>>;
 
-	template <class T, int size, int align> struct FlatImplSelect {
+	template <class T, int size, int align, bool defined_params> struct FlatImplSelect {
 		template <class C>
 		static auto test(int) -> ValidateFlatImpl<C, size, align, sizeof(C), alignof(C)>;
 		template <class C> static auto test(...) -> FlatProxy<T, size, align>;
-
 		using type = decltype(test<T>(0));
-		enum { value = !std::is_same<void, type>::value };
+	};
+
+	template <class T, int size, int align> struct FlatImplSelect<T, size, align, false> {
+		using type = FlatProxy<T, size, align>;
 	};
 }
 
@@ -40,5 +54,6 @@ namespace detail {
 // that T is defined before FlatImpl<T> is instantiated.
 // Limitation: it doesn't work for private members.
 template <class T, int size = type_size<T>, int alignment = 8>
-using FlatImpl = typename detail::FlatImplSelect<T, size, alignment>::type;
+using FlatImpl =
+	typename detail::FlatImplSelect<T, size, alignment, detail::ArgsDefined<T>::value>::type;
 }
