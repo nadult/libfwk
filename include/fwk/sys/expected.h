d01405f50675a09cbcb4bc31c803391766ad34a0
Krzysztof Jakubowski <nadult@fastmail.fm>
2018-01-24 16:52:36 +0100

    Expected<> uses UniquePtr to store Error

--- include/fwk/sys/expected.h
+++ include/fwk/sys/expected.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "fwk/sys/error.h"
+#include "fwk/sys/unique_ptr.h"
 
 namespace fwk {
 
@@ -13,25 +14,25 @@ template <class T> class[[nodiscard]] Expected {
 
 	Expected(const T &value) : m_value(value), m_has_value(true) {}
 	Expected(T &&value) : m_value(move(value)), m_has_value(true) {}
-	Expected(Error error) : m_error(move(error)), m_has_value(false) {}
+	Expected(Error error) : m_error(uniquePtr<Error>(move(error))), m_has_value(false) {}
 	~Expected() {
 		if(m_has_value)
 			m_value.~T();
 		else
-			m_error.~Error();
+			m_error.~UniquePtr<Error>();
 	}
 
 	Expected(const Expected &rhs) : m_has_value(rhs.m_has_value) {
 		if(m_has_value)
 			new(&m_value) T(rhs.m_value);
 		else
-			new(&m_error) Error(rhs.m_error);
+			new(&m_error) UniquePtr<Error>(rhs.m_error);
 	}
 	Expected(Expected &&rhs) : m_has_value(rhs.m_has_value) {
 		if(m_has_value)
 			new(&m_value) T(move(rhs.m_value));
 		else
-			new(&m_error) Error(move(rhs.m_error));
+			new(&m_error) UniquePtr<Error>(move(rhs.m_error));
 	}
 
 	void swap(Expected &rhs) {
@@ -73,24 +74,24 @@ template <class T> class[[nodiscard]] Expected {
 	}
 	Error &error() {
 		DASSERT(!m_has_value);
-		return m_error;
+		return *m_error;
 	}
 
 	const Error &error() const {
 		DASSERT(!m_has_value);
-		return m_error;
+		return *m_error;
 	}
 
 	const T &orElse(const T &on_error) const { return m_has_value ? m_value : on_error; }
 	const T &checked() const {
 		if(!m_has_value)
-			checkFailed(__FILE__, __LINE__, m_error);
+			checkFailed(__FILE__, __LINE__, *m_error);
 		return m_value;
 	}
 
   private:
 	union {
-		Error m_error;
+		UniquePtr<Error> m_error;
 		T m_value;
 	};
 	bool m_has_value;
@@ -98,33 +99,32 @@ template <class T> class[[nodiscard]] Expected {
 
 template <> class[[nodiscard]] Expected<void> {
   public:
-	Expected() : m_has_error(false) {}
-	Expected(Error error) : m_error(move(error)), m_has_error(true) {}
+	Expected() {}
+	Expected(Error error) : m_error(uniquePtr<Error>(move(error))) {}
 
 	void swap(Expected &rhs) {
-		fwk::swap(m_has_error, rhs.m_has_error);
 		fwk::swap(m_error, rhs.m_error);
 	}
 
 	Error &error() {
-		DASSERT(m_has_error);
-		return m_error;
+		DASSERT(m_error.get());
+		return *m_error;
 	}
 
 	const Error &error() const {
-		DASSERT(m_has_error);
-		return m_error;
+		DASSERT(m_error.get());
+		return *m_error;
 	}
 
-	explicit operator bool() const { return !m_has_error; }
+	explicit operator bool() const { return !m_error.get(); }
 
 	void checked() const {
-		if(m_has_error)
-			checkFailed(__FILE__, __LINE__, m_error);
+		if(m_error.get())
+			checkFailed(__FILE__, __LINE__, *m_error);
 	}
 
   private:
-	Error m_error;
-	bool m_has_error;
+	UniquePtr<Error> m_error;
 };
+
 }
