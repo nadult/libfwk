4f7e55c9ca357509d12720d40e744316cd1981be
Krzysztof Jakubowski <nadult@fastmail.fm>
2018-09-18 10:25:00 +0200

    Improved GlQuery

--- include/fwk/gfx/gl_query.h
+++ include/fwk/gfx/gl_query.h
@@ -8,28 +8,33 @@
 
 namespace fwk {
 
+DEFINE_ENUM(GlQueryType, samples_passed, any_samples_passed, any_samples_passed_conservative,
+			primitives_generated, tf_primitives_written, time_elapsed);
+
 class GlQuery {
 	GL_CLASS_DECL(GlQuery)
   public:
+	using Type = GlQueryType;
 	static PQuery make();
 
 	Maybe<int> getValue() const;
 	int waitForValue() const;
 
+	Maybe<Type> type() const { return m_type; }
+	void begin(Type);
+	void end();
+	static void end(Type);
+
   private:
-	bool m_has_value = false;
+	Maybe<Type> m_type;
 	friend struct GlQueryScope;
 };
-
-DEFINE_ENUM(GlQueryType, samples_passed, any_samples_passed, any_samples_passed_conservative,
-			primitives_generated, tf_primitives_written, time_elapsed);
 
 struct GlQueryScope {
 	GlQueryScope(PQuery, GlQueryType);
 	GlQueryScope(const GlQueryScope &) = delete;
 	void operator=(const GlQueryScope &) = delete;
 	~GlQueryScope();
-
 	GlQueryType type;
 };
 
--- src/gfx/gl_query.cpp
+++ src/gfx/gl_query.cpp
@@ -5,6 +5,7 @@
 
 #include "fwk/enum_map.h"
 #include "fwk/gfx/opengl.h"
+#include "fwk/sys/assert.h"
 
 namespace fwk {
 
@@ -13,7 +14,7 @@ GL_CLASS_IMPL(GlQuery)
 PQuery GlQuery::make() { return PQuery(storage.make()); }
 
 Maybe<int> GlQuery::getValue() const {
-	if(!m_has_value)
+	if(!m_type)
 		return none;
 
 	int value = -1;
@@ -22,7 +23,7 @@ Maybe<int> GlQuery::getValue() const {
 }
 
 int GlQuery::waitForValue() const {
-	if(!m_has_value)
+	if(!m_type)
 		return -1;
 	int value = -1;
 	glGetQueryObjectiv(id(), GL_QUERY_RESULT, &value);
@@ -33,10 +34,23 @@ static const EnumMap<GlQueryType, int> s_query_types{
 	{GL_SAMPLES_PASSED, GL_ANY_SAMPLES_PASSED, GL_ANY_SAMPLES_PASSED_CONSERVATIVE,
 	 GL_PRIMITIVES_GENERATED, GL_TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN, GL_TIME_ELAPSED}};
 
+void GlQuery::begin(Type type) {
+	if(m_type)
+		DASSERT_EQ(type, *m_type);
+	m_type = type;
+	glBeginQuery(s_query_types[type], id());
+}
+
+void GlQuery::end() {
+	PASSERT(m_type);
+	glEndQuery(s_query_types[*m_type]);
+}
+
+void GlQuery::end(Type type) { glEndQuery(s_query_types[type]); }
+
 GlQueryScope::GlQueryScope(PQuery query, GlQueryType type) : type(type) {
 	DASSERT(query);
-	glBeginQuery(s_query_types[type], query.id());
-	query->m_has_value = true;
+	query->begin(type);
 }
-GlQueryScope::~GlQueryScope() { glEndQuery(s_query_types[type]); }
+GlQueryScope::~GlQueryScope() { GlQuery::end(type); }
 }
