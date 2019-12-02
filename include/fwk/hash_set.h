9aab7e029bba67a8ad5356ba1c0da9771b4999f0
Krzysztof Jakubowski <nadult@fastmail.fm>
2019-12-02 14:33:06 +0100

    Improved HashSet just like HashMap

--- include/fwk/hash_set.h
+++ include/fwk/hash_set.h
@@ -10,403 +10,320 @@ namespace fwk {
 
 // Original source: hash_map.h from RDE STL by Maciej Sinilo (Copyright 2007)
 // Licensed under MIT license
-//
-// Slightly modified & adapted for libfwk by Krzysztof Jakubowski
-template <class TKey> class HashSet {
+// Improved & adapted for libfwk by Krzysztof Jakubowski
+template <typename TKey> class HashSet {
   public:
+	using Hash = u32;
 	using Key = TKey;
-	using HashValue = u32;
 
-  private:
-	struct Node {
-		// TODO: use single empty value; Use invalid<T>()
-		// TODO: remove hash for small types
-		static constexpr HashValue kUnusedHash = 0xFFFFFFFF;
-		static constexpr HashValue kDeletedHash = 0xFFFFFFFE;
-
-		Node() : hash(kUnusedHash) {}
-		~Node() {}
-
-		bool isUnused() const { return hash == kUnusedHash; }
-		bool isDeleted() const { return hash == kDeletedHash; }
-		bool isOccupied() const { return hash < kDeletedHash; }
-
-		HashValue hash;
-		union {
-			Key data;
-		};
-	};
-	template <bool is_const> class Iterator {
-		friend class HashSet;
-
-	  public:
-		using TNodePtr = If<is_const, const Node *, Node *>;
-		using TPtr = If<is_const, const Key *, Key *>;
-		using TRef = If<is_const, const Key &, Key &>;
-		using iterator_category = std::forward_iterator_tag;
-
-		explicit Iterator(TNodePtr node, const HashSet *map) : m_node(node), m_map(map) {}
-		template <bool rhs_is_const>
-		Iterator(const Iterator<rhs_is_const> &rhs) : m_node(rhs.node()), m_map(rhs.get_map()) {}
-
-		TRef operator*() const {
-			PASSERT(m_node);
-			return m_node->data;
-		}
-		TPtr operator->() const { return &m_node->data; }
-		TNodePtr node() const { return m_node; }
-
-		Iterator &operator++() {
-			PASSERT(m_node);
-			++m_node;
-			move_to_next_occupied_node();
-			return *this;
-		}
-		Iterator operator++(int) {
-			Iterator copy(*this);
-			++(*this);
-			return copy;
+	template <bool is_const> struct TIter {
+		template <bool to_const, EnableIf<!is_const && to_const>...> operator TIter<to_const>() {
+			return {map, idx};
 		}
 
-		bool operator==(const Iterator &rhs) const { return rhs.m_node == m_node; }
+		auto &operator*() const { return PASSERT(!atEnd()), map->m_keys[idx]; }
+		auto *operator-> () const { return PASSERT(!atEnd()), &map->m_keys[idx]; }
+		void operator++() { PASSERT(!atEnd()), ++idx, skipUnoccupied(); }
+		bool operator==(const TIter &rhs) const { return rhs.idx == idx; }
 
-		HashValue hash() const { return m_node->hash; }
-		const HashSet *get_map() const { return m_map; }
+		bool atEnd() const { return idx >= map->m_capacity; }
+		explicit operator bool() const { return idx < map->m_capacity; }
 
-	  private:
-		void move_to_next_occupied_node() {
-			// @todo: save nodeEnd in constructor?
-			TNodePtr nodeEnd = m_map->m_nodes + m_map->capacity();
-			for(/**/; m_node < nodeEnd; ++m_node) {
-				if(m_node->isOccupied())
-					break;
-			}
+		void skipUnoccupied() {
+			while(idx < map->m_capacity && map->m_hashes[idx] >= deleted_hash)
+				idx++;
 		}
-		TNodePtr m_node;
-		const HashSet *m_map;
+
+		If<is_const, const HashSet *, HashSet *> map;
+		int idx;
 	};
 
-  public:
-	using iterator = Iterator<false>;
-	using const_iterator = Iterator<true>;
-	static constexpr int initial_capacity = 64;
-	static_assert(isPowerOfTwo(initial_capacity));
+	using Iter = TIter<false>;
+	using ConstIter = TIter<true>;
 
-	// TODO: move constructors, etc.
 	HashSet() {}
-	explicit HashSet(int initial_bucket_count) { reserve(initial_bucket_count); }
+	explicit HashSet(int min_reserve) { reserve(min_reserve); }
 	HashSet(const HashSet &rhs) { *this = rhs; }
+	HashSet(HashSet &&rhs) { *this = move(rhs); }
 	~HashSet() { deleteNodes(); }
 
-	// Load factor controls hash map load. 4 means 100% load, ie. HashSet will grow
-	// when number of items == capacity. Default value of 6 means it grows when
-	// number of items == capacity * 3/2 (6/4). Higher load == tighter maps, but bigger
-	// risk of collisions.
-	void setLoadFactor4(int load_factor4) { m_load_factor4 = load_factor4; }
-	int loadFactor4() const { return m_load_factor4; }
+	// Load factor controls hash map load. Default is ~66%.
+	// Higher factor means tighter maps and bigger risk of collisions.
+	void setLoadFactor(float factor) {
+		PASSERT(factor >= 0.125f && factor <= 1.0f);
+		m_load_factor = factor;
+		m_used_limit = (int)(m_capacity * factor);
+	}
+	float loadFactor() const { return m_load_factor; }
 
-	iterator begin() {
-		iterator it(m_nodes, this);
-		it.move_to_next_occupied_node();
+	Iter begin() {
+		Iter it{this, 0};
+		it.skipUnoccupied();
 		return it;
 	}
-	const_iterator begin() const {
-		const_iterator it(m_nodes, this);
-		it.move_to_next_occupied_node();
+	ConstIter begin() const {
+		ConstIter it{this, 0};
+		it.skipUnoccupied();
 		return it;
 	}
-	iterator end() { return iterator(m_nodes + m_capacity, this); }
-	const_iterator end() const { return const_iterator(m_nodes + m_capacity, this); }
 
-	bool contains(const Key &key) const { return find(key) != end(); }
+	Iter end() { return {this, m_capacity}; }
+	ConstIter end() const { return {this, m_capacity}; }
 
-	HashSet &operator=(const HashSet &rhs) {
-		checkInvariant();
-		if(&rhs != this) {
-			clear();
-			if(m_capacity < rhs.m_capacity) {
-				deleteNodes();
-				m_nodes = allocateNodes(rhs.m_capacity);
-				m_capacity = rhs.m_capacity;
-				m_capacity_mask = m_capacity - 1;
-			}
-			rehash(m_capacity, m_nodes, rhs.m_capacity, rhs.m_nodes, false);
-			m_size = rhs.size();
-			m_num_used = rhs.m_num_used;
+	bool contains(const Key &key) const { return !!find(key); }
+
+	void operator=(const HashSet &rhs) {
+		if(&rhs == this)
+			return;
+
+		clear();
+		if(m_capacity < rhs.m_capacity) {
+			deleteNodes();
+			m_hashes = allocateHashes(rhs.m_capacity);
+			m_keys = (Key *)fwk::allocate(rhs.m_capacity * sizeof(Key));
+			m_capacity = rhs.m_capacity;
+			m_capacity_mask = m_capacity - 1;
 		}
-		checkInvariant();
-		return *this;
+		rehash(m_capacity, m_hashes, m_keys, rhs.m_capacity, rhs.m_hashes, rhs.m_keys, false);
+		m_size = rhs.size();
+		m_num_used = rhs.m_num_used;
+		setLoadFactor(rhs.m_load_factor);
 	}
+
+	void operator=(HashSet &&rhs) {
+		deleteNodes();
+		memcpy(this, &rhs, sizeof(HashSet));
+		memset(&rhs, 0, sizeof(HashSet));
+		rhs.m_load_factor = m_load_factor;
+	}
+
 	void swap(HashSet &rhs) {
-		if(&rhs != this) {
-			checkInvariant();
-			swap(m_nodes, rhs.m_nodes);
-			swap(m_size, rhs.m_size);
-			swap(m_capacity, rhs.m_capacity);
-			swap(m_capacity_mask, rhs.m_capacity_mask);
-			swap(m_num_used, rhs.m_num_used);
-			checkInvariant();
-		}
+		if(&rhs == this)
+			return;
+		swap(m_hashes, rhs.m_hashes);
+		swap(m_keys, rhs.m_keys);
+		swap(m_size, rhs.m_size);
+		swap(m_capacity, rhs.m_capacity);
+		swap(m_capacity_mask, rhs.m_capacity_mask);
+		swap(m_used_limit, rhs.m_used_limit);
+		swap(m_num_used, rhs.m_num_used);
+		swap(m_load_factor, rhs.m_load_factor);
 	}
 
-	Pair<iterator, bool> emplace(const Key &key) {
-		checkInvariant();
-		if(m_num_used * m_load_factor4 >= m_capacity * 4)
+	Pair<Iter, bool> emplace(const TKey &key) {
+		if(m_num_used >= m_used_limit)
 			grow();
 
-		HashValue hash = hashFunc(key);
-		Node *n = find_for_insert(key, hash);
-		if(n->isOccupied()) {
-			DASSERT(hash == n->hash && key == n->data);
-			return {iterator(n, this), false};
-		}
-		if(n->isUnused())
+		auto hash = hashFunc(key);
+		int idx = findForInsert(key, hash);
+		if(m_hashes[idx] < deleted_hash)
+			return {{this, idx}, false};
+		if(m_hashes[idx] == unused_hash)
 			++m_num_used;
-		new(&n->data) Key(key);
-		n->hash = hash;
+		new(&m_keys[idx]) Key{key};
+		m_hashes[idx] = hash;
 		++m_size;
-		checkInvariant();
-		return {iterator(n, this), true};
+		PASSERT(m_num_used >= m_size);
+		return {{this, idx}, true};
 	}
 
-	int erase(const Key &key) {
-		Node *n = lookup(key);
-		if(n != (m_nodes + m_capacity) && n->isOccupied()) {
-			eraseNode(n);
-			return 1;
+	bool erase(const Key &key) {
+		auto idx = lookup(key);
+		if(idx != m_capacity && m_hashes[idx] < deleted_hash) {
+			eraseNode(idx);
+			return true;
 		}
-		return 0;
+		return false;
 	}
-	void erase(iterator it) {
-		DASSERT(it.get_map() == this);
-		if(it != end()) {
-			DASSERT(!empty());
-			eraseNode(it.node());
-		}
+
+	void erase(Iter it) {
+		PASSERT(valid(it));
+		if(it != end())
+			eraseNode(it.idx);
 	}
-	void erase(iterator from, iterator to) {
-		for(; from != to; ++from) {
-			Node *n = from.node();
-			if(n->isOccupied())
-				eraseNode(n);
+
+	void erase(Iter from, Iter to) {
+		PASSERT(valid(from) && valid(to));
+
+		auto idx = from.idx;
+		while(idx != to.idx) {
+			if(m_hashes[idx] < deleted_hash)
+				eraseNode(idx);
+			++idx;
 		}
 	}
 
-	iterator find(const Key &key) {
-		Node *n = lookup(key);
-		return iterator(n, this);
-	}
-	const_iterator find(const Key &key) const {
-		const Node *n = lookup(key);
-		return const_iterator(n, this);
-	}
+	Iter find(const Key &key) { return {this, lookup(key)}; }
+	ConstIter find(const Key &key) const { return {this, lookup(key)}; }
 
 	void clear() {
-		Node *endNode = m_nodes + m_capacity;
-		for(Node *iter = m_nodes; iter != endNode; ++iter) {
-			if(iter) {
-				if(iter->isOccupied())
-					iter->data.~Key();
-				// We can make them unused, because we clear whole hash_map,
-				// so we can guarantee there'll be no holes.
-				iter->hash = Node::kUnusedHash;
-			}
+		for(int n = 0; n < m_capacity; n++) {
+			if(m_hashes[n] < deleted_hash)
+				m_keys[n].~Key();
+			m_hashes[n] = unused_hash;
 		}
-		m_size = 0;
-		m_num_used = 0;
+		m_size = m_num_used = 0;
 	}
 
-	// TODO: something is wrong here (when reserving space in voxelizer/tri_points procedure
-	// slows down drastically (slower the bigger reserve))
 	void reserve(int min_size) {
-		int newCapacity = (m_capacity == 0 ? initial_capacity : m_capacity);
-		while(newCapacity < min_size)
-			newCapacity *= 2;
-		if(newCapacity > m_capacity)
-			grow(newCapacity);
+		int new_capacity = m_capacity == 0 ? initial_capacity : m_capacity;
+		while(new_capacity < min_size)
+			new_capacity *= 2;
+		if(new_capacity > m_capacity)
+			grow(new_capacity);
 	}
 
 	int capacity() const { return m_capacity; }
 	int size() const { return m_size; }
 	bool empty() const { return size() == 0; }
 
-	int nonemptyBucketCount() const { return m_num_used; }
-	int usedMemory() const { return capacity() * sizeof(Node); }
+	int usedBucketCount() const { return m_num_used; }
+	int usedMemory() const { return capacity() * (sizeof(Hash) + sizeof(Key)); }
 
-	vector<Key> values() const { return keys(); }
-	vector<Key> keys() const {
-		vector<Key> out;
-		out.reserve(size());
-		for(auto &pair : *this)
-			out.emplace_back(pair.first);
-		return out;
+	CSpan<Hash> hashes() const { return {m_hashes, m_capacity}; }
+	Key *rawData() { return m_keys; }
+	const Key *rawData() const { return m_keys; }
+
+	vector<Key> keys() const { return transform<Key>(*this); }
+
+	template <bool is_const> bool valid(TIter<is_const> iter) const {
+		return iter.idx >= 0 && iter.idx <= m_capacity && iter.map == this;
 	}
 
   private:
-	void grow() {
-		const int newCapacity = (m_capacity == 0 ? initial_capacity : m_capacity * 2);
-		grow(newCapacity);
-	}
+	static constexpr int initial_capacity = 64;
+	static_assert(isPowerOfTwo(initial_capacity));
+
+	// Index is occupied if hash < deleted_hash
+	static constexpr Hash unused_hash = 0xffffffff;
+	static constexpr Hash deleted_hash = 0xfffffffe;
+
+	void grow() { grow(m_capacity == 0 ? initial_capacity : m_capacity * 2); }
 	void grow(int new_capacity) {
-		DASSERT(isPowerOfTwo(new_capacity));
-		Node *newNodes = allocateNodes(new_capacity);
-		rehash(new_capacity, newNodes, m_capacity, m_nodes, true);
-		if(m_nodes != &s_empty_node)
-			fwk::deallocate(m_nodes);
+		PASSERT(isPowerOfTwo(new_capacity));
+		auto *new_hashes = allocateHashes(new_capacity);
+		auto *new_keys = (Key *)fwk::allocate(new_capacity * sizeof(Key));
+
+		rehash(new_capacity, new_hashes, new_keys, m_capacity, m_hashes, m_keys, true);
+		if(m_hashes != &s_empty_node)
+			fwk::deallocate(m_hashes);
+		fwk::deallocate(m_keys);
+
 		m_capacity = new_capacity;
 		m_capacity_mask = new_capacity - 1;
-		m_nodes = newNodes;
+		m_used_limit = (int)(m_capacity * m_load_factor);
+		m_hashes = new_hashes;
+		m_keys = new_keys;
 		m_num_used = m_size;
-		DASSERT(m_num_used < m_capacity);
+		PASSERT(m_num_used < m_capacity);
 	}
-	Pair<iterator, bool> emplace_at(const Key &v, Node *n, HashValue hash) {
-		checkInvariant();
-		if(n == 0 || m_num_used * m_load_factor4 >= m_capacity * 4)
+
+	Pair<Iter, bool> emplaceAt(const Key &v, int idx, Hash hash) {
+		if(idx == m_capacity || m_num_used >= m_used_limit)
 			return emplace(v);
 
-		DASSERT(!n->isOccupied());
-		if(n->isUnused())
+		PASSERT(m_hashes[idx] >= deleted_hash);
+		if(m_hashes[idx] == unused_hash)
 			++m_num_used;
-		new(&n->data) Key(v);
-		n->hash = hash;
+		new(&m_keys[idx]) Key(v);
+		m_hashes[idx] = hash;
 		++m_size;
-		checkInvariant();
-		return {iterator(n, this), true};
+		return {{this, idx}, true};
 	}
-	Node *find_for_insert(const Key &key, HashValue hash) {
-		if(m_capacity == 0)
-			return 0;
 
-		u32 i = hash & m_capacity_mask;
+	int findForInsert(const Key &key, Hash hash) {
+		int idx = hash & m_capacity_mask;
+		if(m_hashes[idx] == hash && m_keys[idx] == key)
+			return idx;
+		if(idx == m_capacity)
+			return m_capacity;
 
-		Node *n = m_nodes + i;
-		if(n->hash == hash && key == n->data)
-			return n;
+		int free_idx = m_hashes[idx] == deleted_hash ? idx : -1;
 
-		Node *freeNode(0);
-		if(n->isDeleted())
-			freeNode = n;
-		u32 numProbes(0);
 		// Guarantees loop termination.
-		DASSERT(m_num_used < m_capacity);
-		while(!n->isUnused()) {
-			// TODO: try quadratic probing?
-			++numProbes;
-			i = (i + numProbes) & m_capacity_mask;
-			n = m_nodes + i;
-			if(compare_key(n, key, hash))
-				return n;
-			if(n->isDeleted() && freeNode == 0)
-				freeNode = n;
-		}
-		return freeNode ? freeNode : n;
-	}
-	Node *lookup(const Key &key) const {
-		const HashValue hash = hashFunc(key);
-		u32 i = hash & m_capacity_mask;
-		Node *n = m_nodes + i;
-		if(n->hash == hash && key == n->data)
-			return n;
+		PASSERT(m_num_used < m_capacity);
 
-		u32 numProbes(0);
+		int num_probes = 1;
+		while(m_hashes[idx] <= deleted_hash) {
+			idx = (idx + num_probes++) & m_capacity_mask;
+			if(m_hashes[idx] == hash && m_keys[idx] == key)
+				return idx;
+			if(m_hashes[idx] == deleted_hash && free_idx == -1)
+				free_idx = idx;
+		}
+		return free_idx != -1 ? free_idx : idx;
+	}
+
+	int lookup(const Key &key) const {
+		auto hash = hashFunc(key);
+		unsigned idx = hash & m_capacity_mask;
+		if(m_hashes[idx] == hash && m_keys[idx] == key)
+			return idx;
+
 		// Guarantees loop termination.
-		DASSERT(m_capacity == 0 || m_num_used < m_capacity);
-		while(!n->isUnused()) {
-			++numProbes;
-			i = (i + numProbes) & m_capacity_mask;
-			n = m_nodes + i;
+		PASSERT(m_capacity == 0 || m_num_used < m_capacity);
 
-			if(compare_key(n, key, hash))
-				return n;
+		int num_probes = 1;
+		while(m_hashes[idx] <= deleted_hash) {
+			idx = (idx + num_probes++) & m_capacity_mask;
+			if(m_hashes[idx] == hash && m_keys[idx] == key)
+				return idx;
 		}
-		return m_nodes + m_capacity;
+		return m_capacity;
 	}
 
-	static void rehash(int new_capacity, Node *new_nodes, int capacity, const Node *nodes,
-					   bool destruct_original) {
-		//if (nodes == &s_empty_node || new_nodes == &s_empty_node)
-		//  return;
-
-		const Node *it = nodes;
-		const Node *itEnd = nodes + capacity;
+	static void rehash(int new_capacity, Hash *new_hashes, Key *new_keys, int capacity,
+					   const Hash *hashes, const Key *keys, bool destruct_original) {
 		const u32 mask = new_capacity - 1;
-		while(it != itEnd) {
-			if(it->isOccupied()) {
-				const HashValue hash = it->hash;
+		for(int idx = 0; idx < capacity; idx++) {
+			if(hashes[idx] < deleted_hash) {
+				const Hash hash = hashes[idx];
 				u32 i = hash & mask;
 
-				Node *n = new_nodes + i;
-				u32 numProbes(0);
-				while(!n->isUnused()) {
-					++numProbes;
-					i = (i + numProbes) & mask;
-					n = new_nodes + i;
-				}
-				new(&n->data) Key(it->data);
-				n->hash = hash;
+				int num_probes = 1;
+				while(new_hashes[i] != unused_hash)
+					i = (i + num_probes++) & mask;
+				new(&new_keys[i]) Key(keys[idx]);
+				new_hashes[i] = hash;
 				if(destruct_original)
-					it->data.~Key();
+					keys[idx].~Key();
 			}
-			++it;
 		}
 	}
 
-	Node *allocateNodes(int n) {
-		Node *buckets = static_cast<Node *>(fwk::allocate(n * sizeof(Node)));
-		Node *iterBuckets(buckets);
-		Node *end = iterBuckets + n;
-		for(/**/; iterBuckets != end; ++iterBuckets)
-			iterBuckets->hash = Node::kUnusedHash;
-
-		return buckets;
+	Hash *allocateHashes(int count) {
+		auto *hashes = static_cast<Hash *>(fwk::allocate(count * sizeof(Hash)));
+		for(int n = 0; n < count; n++)
+			hashes[n] = unused_hash;
+		return hashes;
 	}
+
 	void deleteNodes() {
-		Node *it = m_nodes;
-		Node *itEnd = it + m_capacity;
-		while(it != itEnd) {
-			if(it && it->isOccupied())
-				it->data.~Key();
-			++it;
-		}
-		if(m_nodes != &s_empty_node)
-			fwk::deallocate(m_nodes);
-
-		m_capacity = 0;
-		m_capacity_mask = 0;
-		m_size = 0;
+		for(int n = 0; n < m_capacity; n++)
+			if(m_hashes[n] < deleted_hash)
+				m_keys[n].~Key();
+		if(m_hashes != &s_empty_node)
+			fwk::deallocate(m_hashes);
+		fwk::deallocate(m_keys);
+		m_capacity = m_size = m_capacity_mask = m_used_limit = 0;
 	}
 
-	void eraseNode(Node *n) {
-		DASSERT(!empty());
-		DASSERT(n->isOccupied());
-		n->data.~Key();
-		n->hash = Node::kDeletedHash;
+	void eraseNode(int idx) {
+		PASSERT(m_hashes[idx] < deleted_hash);
+		m_keys[idx].~Key();
+		m_hashes[idx] = deleted_hash;
 		--m_size;
 	}
 
-	HashValue hashFunc(const Key &key) const {
-		const HashValue h = hash(key) & 0xFFFFFFFD;
-		//DASSERT(h < node::kDeletedHash);
-		return h;
-	}
-	void checkInvariant() const {
-		PASSERT(isPowerOfTwo(m_capacity));
-		PASSERT(m_num_used >= m_size);
-	}
+	Hash hashFunc(const Key &key) const { return hash<Hash>(key) & 0x7FFFFFFFu; }
 
-	bool compare_key(const Node *n, const Key &key, HashValue hash) const {
-		return n->hash == hash && key == n->data;
-	}
-
-	static Node s_empty_node;
-
-	Node *m_nodes = &s_empty_node;
-	int m_size = 0;
-	int m_capacity = 0;
+	static inline Hash s_empty_node = unused_hash;
+	Hash *m_hashes = &s_empty_node;
+	Key *m_keys = nullptr;
+	int m_size = 0, m_capacity = 0;
+	int m_num_used = 0, m_used_limit = 0;
+	float m_load_factor = 2.0f / 3.0f;
 	u32 m_capacity_mask = 0;
-	int m_num_used = 0;
-	int m_load_factor4 = 6;
+	template <bool is_const> friend struct TIter;
 };
-
-template <typename TKey> typename HashSet<TKey>::Node HashSet<TKey>::s_empty_node;
 }
