d63944ac300894ba17147f85cc94a3bb23b42476
Krzysztof Jakubowski <nadult@fastmail.fm>
2020-01-21 20:58:53 +0100

    Fixed compilation of UrlFetch

--- src/io/url_fetch.cpp
+++ src/io/url_fetch.cpp
@@ -21,19 +21,24 @@ struct UrlFetch::Impl {
 	}
 
 	static void downloadSucceeded(emscripten_fetch_t *fetch) {
-		status = Status::completed;
-		data = {(const char *)fetch->data, fetch->numBytes};
-		progress.butes_downloaded = progress.bytes_total = fetch->numBytes;
-		ermscripten_fetch_close(fetch);
+		auto &impl = *(Impl *)fetch->userData;
+		impl.status = Status::completed;
+		impl.data = cspan((const char *)fetch->data, fetch->numBytes);
+		impl.progress.bytes_downloaded = impl.progress.bytes_total = fetch->numBytes;
+		emscripten_fetch_close(fetch);
 	}
 
 	static void downloadFailed(emscripten_fetch_t *fetch) {
-		status = Status::failed;
-		updateProgress(*fetch);
-		ermscripten_fetch_close(fetch);
+		auto &impl = *(Impl *)fetch->userData;
+		impl.status = Status::failed;
+		impl.updateProgress(*fetch);
+		emscripten_fetch_close(fetch);
 	}
 
-	static void downloadProgress(emscripten_fetch_t *fetch) { updateProgress(*fetch); }
+	static void downloadProgress(emscripten_fetch_t *fetch) {
+		auto &impl = *(Impl *)fetch->userData;
+		impl.updateProgress(*fetch);
+	}
 #endif
 };
 
@@ -42,7 +47,9 @@ Ex<UrlFetch> UrlFetch::make(ZStr relative_url) {
 	emscripten_fetch_attr_t attr;
 	emscripten_fetch_attr_init(&attr);
 	strcpy(attr.requestMethod, "GET");
-	attr.userData = m_impl.get();
+
+	UrlFetch out;
+	attr.userData = out.m_impl.get();
 	attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
 	attr.onsuccess = Impl::downloadSucceeded;
 	attr.onerror = Impl::downloadFailed;
@@ -50,7 +57,8 @@ Ex<UrlFetch> UrlFetch::make(ZStr relative_url) {
 	auto *fetch = emscripten_fetch(&attr, relative_url.c_str());
 	if(!fetch)
 		return ERROR("Error while initiating URLFetch");
-	m_impl->updateProgress(fetch);
+	out.m_impl->updateProgress(*fetch);
+	return out;
 #else
 	return ERROR("UrlFetch supported only on HTML platform (for now)");
 #endif
@@ -60,9 +68,9 @@ Ex<vector<char>> UrlFetch::finish(UrlFetch fetch) {
 #ifdef FWK_PLATFORM_HTML
 	auto data = move(fetch.m_impl->data);
 	if(fetch.m_impl->status != Status::completed)
-		return
-			// TODO: handle errors
-			return data;
+		return ERROR("Cannot finish UrlFetch: %",
+					 fetch.status() == Status::failed ? "fetch failed" : "still downloading");
+	return data;
 #else
 	return ERROR("UrlFetch supported only on HTML platform (for now)");
 #endif
