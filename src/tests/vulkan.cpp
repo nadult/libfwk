aac9eb1450671f8a5885e9e0b8bb73ada1c2bf6e
Krzysztof Jakubowski <nadult@fastmail.fm>
2022-06-20 12:47:52 +0200

    Vulkan: added pipeline creation to test (WIP)

--- src/tests/vulkan.cpp
+++ src/tests/vulkan.cpp
@@ -14,6 +14,14 @@
 #include <fwk/vulkan/vulkan_instance.h>
 #include <fwk/vulkan/vulkan_window.h>
 
+#include <vulkan/vulkan.h>
+
+// TODO: handling VkResults
+// TODO: making sure that handles are correct ?
+// TODO: more type safety
+// TODO: VHandle<> class ? handles are not initialized to null by default ...
+// TODO: kolejnoœæ niszczenia obiektów...
+
 using namespace fwk;
 
 const char *vertex_shader = R"(
@@ -79,6 +87,217 @@ string fontPath() {
 	return findDefaultSystemFont().get().file_path;
 }
 
+Ex<VkShaderModule> createShaderModule(VkDevice device_handle, CSpan<char> bytecode) {
+	VkShaderModuleCreateInfo ci{};
+	ci.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	ci.codeSize = bytecode.size();
+	// TODO: make sure that this is safe
+	ci.pCode = reinterpret_cast<const uint32_t *>(bytecode.data());
+
+	VkShaderModule handle;
+	if(vkCreateShaderModule(device_handle, &ci, nullptr, &handle) != VK_SUCCESS)
+		return ERROR("vkCreateShaderModule failed");
+	return handle;
+}
+
+struct Pipeline {
+	VkPipeline graphicsPipeline;
+	VkRenderPass renderPass;
+	VkPipelineLayout pipelineLayout;
+};
+
+Ex<Pipeline> createPipeline(VDeviceId device_id, const VulkanSwapChainInfo &swap_chain) {
+	Pipeline out;
+
+	auto &instance = VulkanInstance::instance();
+	auto device_handle = instance[device_id].handle;
+
+	// TODO: making sure that shaderc_shared.dll is available
+	ShaderCompiler compiler;
+	auto vsh_code = compiler.compile(ShaderType::vertex, vertex_shader);
+	auto fsh_code = compiler.compile(ShaderType::fragment, fragment_shader);
+
+	if(!vsh_code.bytecode)
+		return ERROR("Failed to compile vertex shader:\n%", vsh_code.messages);
+	if(!fsh_code.bytecode)
+		return ERROR("Failed to compile fragment shader:\n%", fsh_code.messages);
+
+	auto vsh_module = EX_PASS(createShaderModule(device_handle, vsh_code.bytecode));
+	auto fsh_module = EX_PASS(createShaderModule(device_handle, fsh_code.bytecode));
+
+	VkPipelineShaderStageCreateInfo vsh_stage_ci{};
+	vsh_stage_ci.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	vsh_stage_ci.stage = VK_SHADER_STAGE_VERTEX_BIT;
+	vsh_stage_ci.module = vsh_module;
+	vsh_stage_ci.pName = "main";
+
+	VkPipelineShaderStageCreateInfo fsh_stage_ci{};
+	fsh_stage_ci.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	fsh_stage_ci.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	fsh_stage_ci.module = fsh_module;
+	fsh_stage_ci.pName = "main";
+
+	VkPipelineShaderStageCreateInfo shader_stages_ci[] = {vsh_stage_ci, fsh_stage_ci};
+
+	VkPipelineVertexInputStateCreateInfo vertex_input_ci{};
+	vertex_input_ci.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+	vertex_input_ci.vertexBindingDescriptionCount = 0;
+	vertex_input_ci.pVertexBindingDescriptions = nullptr; // Optional
+	vertex_input_ci.vertexAttributeDescriptionCount = 0;
+	vertex_input_ci.pVertexAttributeDescriptions = nullptr; // Optional
+
+	VkPipelineInputAssemblyStateCreateInfo input_assembly_ci{};
+	input_assembly_ci.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+	input_assembly_ci.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+	input_assembly_ci.primitiveRestartEnable = VK_FALSE;
+
+	VkViewport viewport{};
+	viewport.x = 0.0f;
+	viewport.y = 0.0f;
+	viewport.width = (float)swap_chain.extent.width;
+	viewport.height = (float)swap_chain.extent.height;
+	viewport.minDepth = 0.0f;
+	viewport.maxDepth = 1.0f;
+
+	VkRect2D scissor{};
+	scissor.offset = {0, 0};
+	scissor.extent = swap_chain.extent;
+
+	VkPipelineViewportStateCreateInfo viewport_state_ci{};
+	viewport_state_ci.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+	viewport_state_ci.viewportCount = 1;
+	viewport_state_ci.pViewports = &viewport;
+	viewport_state_ci.scissorCount = 1;
+	viewport_state_ci.pScissors = &scissor;
+
+	VkPipelineRasterizationStateCreateInfo rasterizer_ci{};
+	rasterizer_ci.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+	rasterizer_ci.depthClampEnable = VK_FALSE;
+	rasterizer_ci.rasterizerDiscardEnable = VK_FALSE;
+	rasterizer_ci.polygonMode = VK_POLYGON_MODE_FILL;
+	rasterizer_ci.lineWidth = 1.0f;
+	rasterizer_ci.cullMode = VK_CULL_MODE_BACK_BIT;
+	rasterizer_ci.frontFace = VK_FRONT_FACE_CLOCKWISE;
+	rasterizer_ci.depthBiasEnable = VK_FALSE;
+	rasterizer_ci.depthBiasConstantFactor = 0.0f; // Optional
+	rasterizer_ci.depthBiasClamp = 0.0f; // Optional
+	rasterizer_ci.depthBiasSlopeFactor = 0.0f; // Optional
+
+	VkPipelineMultisampleStateCreateInfo multisampling{};
+	multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+	multisampling.sampleShadingEnable = VK_FALSE;
+	multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+	multisampling.minSampleShading = 1.0f; // Optional
+	multisampling.pSampleMask = nullptr; // Optional
+	multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
+	multisampling.alphaToOneEnable = VK_FALSE; // Optional
+
+	VkPipelineColorBlendAttachmentState color_blend_attachment{};
+	color_blend_attachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+											VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+	color_blend_attachment.blendEnable = VK_FALSE;
+	color_blend_attachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+	color_blend_attachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+	color_blend_attachment.colorBlendOp = VK_BLEND_OP_ADD; // Optional
+	color_blend_attachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+	color_blend_attachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+	color_blend_attachment.alphaBlendOp = VK_BLEND_OP_ADD; // Optional
+
+	VkPipelineColorBlendStateCreateInfo colorBlending{};
+	colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+	colorBlending.logicOpEnable = VK_FALSE;
+	colorBlending.logicOp = VK_LOGIC_OP_COPY; // Optional
+	colorBlending.attachmentCount = 1;
+	colorBlending.pAttachments = &color_blend_attachment;
+	colorBlending.blendConstants[0] = 0.0f; // Optional
+	colorBlending.blendConstants[1] = 0.0f; // Optional
+	colorBlending.blendConstants[2] = 0.0f; // Optional
+	colorBlending.blendConstants[3] = 0.0f; // Optional
+
+	vector<VkDynamicState> dynamicStates = {VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_LINE_WIDTH};
+
+	VkPipelineDynamicStateCreateInfo dynamicState{};
+	dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+	dynamicState.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
+	dynamicState.pDynamicStates = dynamicStates.data();
+
+	VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+	pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+	pipelineLayoutInfo.setLayoutCount = 0; // Optional
+	pipelineLayoutInfo.pSetLayouts = nullptr; // Optional
+	pipelineLayoutInfo.pushConstantRangeCount = 0; // Optional
+	pipelineLayoutInfo.pPushConstantRanges = nullptr; // Optional
+
+	if(vkCreatePipelineLayout(device_handle, &pipelineLayoutInfo, nullptr, &out.pipelineLayout) !=
+	   VK_SUCCESS)
+		return ERROR("vkCreatePipelineLayout failed");
+
+	VkAttachmentDescription colorAttachment{};
+	colorAttachment.format = swap_chain.format;
+	colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+	colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+	VkAttachmentReference colorAttachmentRef{};
+	colorAttachmentRef.attachment = 0;
+	colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	VkSubpassDescription subpass{};
+	subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+	subpass.colorAttachmentCount = 1;
+	subpass.pColorAttachments = &colorAttachmentRef;
+
+	VkRenderPassCreateInfo renderPassInfo{};
+	renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	renderPassInfo.attachmentCount = 1;
+	renderPassInfo.pAttachments = &colorAttachment;
+	renderPassInfo.subpassCount = 1;
+	renderPassInfo.pSubpasses = &subpass;
+
+	if(vkCreateRenderPass(device_handle, &renderPassInfo, nullptr, &out.renderPass) != VK_SUCCESS) {
+		return ERROR("vkCreateRenderPass failed");
+	}
+
+	VkGraphicsPipelineCreateInfo pipelineInfo{};
+	pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+	pipelineInfo.stageCount = 2;
+	pipelineInfo.pStages = shader_stages_ci;
+	pipelineInfo.pVertexInputState = &vertex_input_ci;
+	pipelineInfo.pInputAssemblyState = &input_assembly_ci;
+	pipelineInfo.pViewportState = &viewport_state_ci;
+	pipelineInfo.pRasterizationState = &rasterizer_ci;
+	pipelineInfo.pMultisampleState = &multisampling;
+	pipelineInfo.pDepthStencilState = nullptr; // Optional
+	pipelineInfo.pColorBlendState = &colorBlending;
+	pipelineInfo.pDynamicState = nullptr; // Optional
+	pipelineInfo.layout = out.pipelineLayout;
+	pipelineInfo.renderPass = out.renderPass;
+	pipelineInfo.subpass = 0;
+	pipelineInfo.basePipelineHandle = VK_NULL_HANDLE; // Optional
+	pipelineInfo.basePipelineIndex = -1; // Optional
+
+	if(vkCreateGraphicsPipelines(device_handle, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr,
+								 &out.graphicsPipeline) != VK_SUCCESS) {
+		return ERROR("vkCreateGraphicsPipelines failed");
+	}
+
+	// TODO: proper destruction
+	vkDestroyShaderModule(device_handle, vsh_module, nullptr);
+	vkDestroyShaderModule(device_handle, fsh_module, nullptr);
+
+	return out;
+}
+
+void destroyGraphicsPipeline(VkDevice device_handle, Pipeline &pipeline) {
+	vkDestroyPipeline(device_handle, pipeline.graphicsPipeline, nullptr);
+	vkDestroyRenderPass(device_handle, pipeline.renderPass, nullptr);
+	vkDestroyPipelineLayout(device_handle, pipeline.pipelineLayout, nullptr);
+}
+
 Ex<int> exMain() {
 	double time = getTime();
 
@@ -90,28 +309,25 @@ Ex<int> exMain() {
 		EXPECT(instance.initialize(setup));
 	}
 
-	// TODO: making sure that shaderc_shared.dll is available
-	ShaderCompiler compiler;
-	auto vsh_compiled = compiler.compile(ShaderType::vertex, vertex_shader);
-	auto fsh_compled = compiler.compile(ShaderType::fragment, fragment_shader);
-
 	auto flags = VWindowFlag::resizable | VWindowFlag::vsync | VWindowFlag::centered |
 				 VWindowFlag::allow_hidpi;
 	auto window = EX_PASS(construct<VulkanWindow>("fwk::vulkan_test", IRect(0, 0, 1280, 720),
 												  VulkanWindowConfig{flags}));
 
-	{
-		VulkanDeviceSetup setup;
-		auto pref_device = instance.preferredDevice(window.surfaceHandle(), &setup.queues);
-		if(!pref_device)
-			return ERROR("Coudln't find a suitable Vulkan device");
-		auto device_id = EX_PASS(instance.createDevice(*pref_device, setup));
-		EXPECT(window.createSwapChain(device_id));
-	}
+	VulkanDeviceSetup setup;
+	auto pref_device = instance.preferredDevice(window.surfaceHandle(), &setup.queues);
+	if(!pref_device)
+		return ERROR("Coudln't find a suitable Vulkan device");
+	auto device_id = EX_PASS(instance.createDevice(*pref_device, setup));
+	EXPECT(window.createSwapChain(device_id));
+
+	auto pipeline = EX_PASS(createPipeline(device_id, window.swapChain()));
 
 	int font_size = 16 * window.dpiScale();
 	//auto font = FontFactory().makeFont(fontPath(), font_size);
 	window.runMainLoop(mainLoop, nullptr); //&font.get());
+
+	destroyGraphicsPipeline(instance[device_id].handle, pipeline);
 
 	return 0;
 }
