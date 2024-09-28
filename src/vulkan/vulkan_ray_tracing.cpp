d59a0e4fce550061052c91b82c143b62285e3478
Krzysztof Jakubowski <nadult@fastmail.fm>
2024-09-28 11:54:11 +0200

    Ray tracing: added function to build TLAS

--- src/vulkan/vulkan_ray_tracing.cpp
+++ src/vulkan/vulkan_ray_tracing.cpp
@@ -13,8 +13,8 @@
 namespace fwk {
 
 VulkanAccelStruct::VulkanAccelStruct(VkAccelerationStructureKHR handle, VObjectId id,
-									 PVBuffer buffer)
-	: VulkanObjectBase(handle, id), m_buffer(std::move(buffer)) {}
+									 PVBuffer buffer, VAccelStructType type)
+	: VulkanObjectBase(handle, id), m_buffer(std::move(buffer)), m_type(type) {}
 
 VulkanAccelStruct::~VulkanAccelStruct() {
 	deferredRelease(vkDestroyAccelerationStructureKHR, m_handle);
@@ -34,7 +34,7 @@ Ex<PVAccelStruct> VulkanAccelStruct::create(VulkanDevice &device, VAccelStructTy
 	VkAccelerationStructureKHR handle = 0;
 	FWK_VK_EXPECT_CALL(vkCreateAccelerationStructureKHR, device.handle(), &create_info, nullptr,
 					   &handle);
-	return device.createObject(handle, buffer.buffer());
+	return device.createObject(handle, buffer.buffer(), type);
 }
 
 static u64 getAddress(VulkanDevice &device, const VulkanBuffer &buffer) {
@@ -47,8 +47,8 @@ template <class T> static u64 getAddress(VulkanDevice &device, VBufferSpan<T> bu
 	return getAddress(device, *buffer.buffer()) + buffer.byteOffset();
 }
 
-Ex<PVAccelStruct> VulkanAccelStruct::build(VulkanDevice &device, VBufferSpan<float3> vertices,
-										   VBufferSpan<u32> indices) {
+Ex<PVAccelStruct> VulkanAccelStruct::buildBottom(VulkanDevice &device, VBufferSpan<float3> vertices,
+												 VBufferSpan<u32> indices) {
 	DASSERT(vertices.buffer()->usage() & VBufferUsage::accel_struct_build_input_read_only);
 	DASSERT(indices.buffer()->usage() & VBufferUsage::accel_struct_build_input_read_only);
 
@@ -68,12 +68,12 @@ Ex<PVAccelStruct> VulkanAccelStruct::build(VulkanDevice &device, VBufferSpan<flo
 	geometry.flags = VK_GEOMETRY_OPAQUE_BIT_KHR;
 	geometry.geometry.triangles = triangles;
 
-	VkAccelerationStructureBuildRangeInfoKHR build_range_info;
-	build_range_info.firstVertex = vertices.byteOffset() / sizeof(float3);
-	build_range_info.primitiveCount = indices.size() / 3;
-	build_range_info.primitiveOffset = 0;
-	build_range_info.transformOffset = 0;
-	auto *build_range_infos = &build_range_info;
+	VkAccelerationStructureBuildRangeInfoKHR range_info;
+	range_info.firstVertex = vertices.byteOffset() / sizeof(float3);
+	range_info.primitiveCount = indices.size() / 3;
+	range_info.primitiveOffset = 0;
+	range_info.transformOffset = 0;
+	auto *range_infos = &range_info;
 
 	VkAccelerationStructureBuildGeometryInfoKHR build_info{
 		VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_GEOMETRY_INFO_KHR};
@@ -91,8 +91,9 @@ Ex<PVAccelStruct> VulkanAccelStruct::build(VulkanDevice &device, VBufferSpan<flo
 											VK_ACCELERATION_STRUCTURE_BUILD_TYPE_DEVICE_KHR,
 											&build_info, &prim_count, &size_info);
 
-	auto buffer = EX_PASS(VulkanBuffer::create(device, size_info.accelerationStructureSize,
-											   VBufferUsage::accel_struct_storage));
+	auto buffer = EX_PASS(
+		VulkanBuffer::create(device, size_info.accelerationStructureSize,
+							 VBufferUsage::device_address | VBufferUsage::accel_struct_storage));
 	auto accel = EX_PASS(VulkanAccelStruct::create(device, VAccelStructType::bottom_level, buffer));
 
 	accel->m_scratch_buffer =
@@ -105,12 +106,90 @@ Ex<PVAccelStruct> VulkanAccelStruct::build(VulkanDevice &device, VBufferSpan<flo
 
 	auto &cmds = device.cmdQueue();
 	auto cmd_handle = cmds.bufferHandle();
-	vkCmdBuildAccelerationStructuresKHR(cmd_handle, 1, &build_info, &build_range_infos);
+	vkCmdBuildAccelerationStructuresKHR(cmd_handle, 1, &build_info, &range_infos);
+
 	// TODO: barriers here in case of multiple builds
 	// TODO: compacting
 	// TODO: clear scratch buffer once building is done
+	// TODO: add option to wait until acceleration structure is built
 
 	return accel;
 }
 
+Ex<PVAccelStruct> VulkanAccelStruct::buildTop(VulkanDevice &device,
+											  CSpan<VAccelStructInstance> instances) {
+	auto vk_instances = transform(instances, [](const VAccelStructInstance &instance) {
+		DASSERT(instance.as);
+		VkAccelerationStructureInstanceKHR out{};
+		for(int row = 0; row < 4; row++)
+			for(int col = 0; col < 3; col++)
+				out.transform.matrix[col][row] = instance.transform[row][col];
+		out.accelerationStructureReference = instance.as->deviceAddress();
+		out.flags = VK_GEOMETRY_INSTANCE_TRIANGLE_FACING_CULL_DISABLE_BIT_KHR;
+		out.mask = 0xff;
+		return out;
+	});
+
+	auto instances_buffer = VulkanBuffer::createAndUpload(
+		device, vk_instances,
+		VBufferUsage::device_address | VBufferUsage::accel_struct_build_input_read_only |
+			VBufferUsage::transfer_dst);
+
+	VkAccelerationStructureGeometryKHR as_geometry = {
+		VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_KHR};
+	as_geometry.geometryType = VK_GEOMETRY_TYPE_INSTANCES_KHR;
+	as_geometry.flags = VK_GEOMETRY_OPAQUE_BIT_KHR;
+	as_geometry.geometry.instances.sType =
+		VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_INSTANCES_DATA_KHR;
+	as_geometry.geometry.instances.arrayOfPointers = VK_FALSE;
+	as_geometry.geometry.instances.data.deviceAddress = getAddress(device, *instances_buffer);
+
+	VkAccelerationStructureBuildGeometryInfoKHR build_info{
+		VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_GEOMETRY_INFO_KHR};
+	build_info.type = VK_ACCELERATION_STRUCTURE_TYPE_TOP_LEVEL_KHR;
+	build_info.flags = VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR;
+	build_info.geometryCount = 1;
+	build_info.pGeometries = &as_geometry;
+
+	uint32_t primitive_count = 1;
+	VkAccelerationStructureBuildSizesInfoKHR size_info{
+		VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_SIZES_INFO_KHR};
+	vkGetAccelerationStructureBuildSizesKHR(device.handle(),
+											VK_ACCELERATION_STRUCTURE_BUILD_TYPE_DEVICE_KHR,
+											&build_info, &primitive_count, &size_info);
+
+	auto buffer = EX_PASS(VulkanBuffer::create(device, size_info.accelerationStructureSize,
+											   VBufferUsage::accel_struct_storage));
+	auto accel = EX_PASS(VulkanAccelStruct::create(device, VAccelStructType::top_level, buffer));
+
+	accel->m_scratch_buffer =
+		EX_PASS(VulkanBuffer::create(device, size_info.buildScratchSize,
+									 VBufferUsage::device_address | VBufferUsage::storage_buffer));
+	build_info.scratchData.deviceAddress = getAddress(device, *accel->m_scratch_buffer);
+	build_info.mode = VK_BUILD_ACCELERATION_STRUCTURE_MODE_BUILD_KHR;
+	build_info.dstAccelerationStructure = accel.handle();
+
+	VkAccelerationStructureBuildRangeInfoKHR range_info{};
+	range_info.primitiveCount = 1;
+	range_info.primitiveOffset = 0;
+	range_info.firstVertex = 0;
+	range_info.transformOffset = 0;
+	auto *range_infos = &range_info;
+
+	auto &cmds = device.cmdQueue();
+	auto cmd_handle = cmds.bufferHandle();
+	vkCmdBuildAccelerationStructuresKHR(cmd_handle, 1, &build_info, &range_infos);
+
+	// TODO: add option to wait until acceleration structure is built
+	// TODO: create custom command buffer and run build commands immediately?
+
+	return accel;
+}
+
+VkDeviceAddress VulkanAccelStruct::deviceAddress() const {
+	VkAccelerationStructureDeviceAddressInfoKHR address_info{
+		VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_DEVICE_ADDRESS_INFO_KHR};
+	address_info.accelerationStructure = handle();
+	return vkGetAccelerationStructureDeviceAddressKHR(deviceHandle(), &address_info);
+}
 }
