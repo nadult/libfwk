5c587906c1d44410710b49aa0cd9d34b58c5474e
Krzysztof Jakubowski <nadult@fastmail.fm>
2024-09-29 16:59:59 +0200

    Formatted install_deps.py

--- tools/install_deps.py
+++ tools/install_deps.py
@@ -28,10 +28,10 @@ class PackageQuery:
 
 
 def parse_packages_json(query: str, json_text: str) -> list[PackageInfo]:
-    local_cache = json.loads(json_text)['Local Cache']
-    if 'error' in local_cache:
+    local_cache = json.loads(json_text)["Local Cache"]
+    if "error" in local_cache:
         return []
-    revs = local_cache[query.pattern()]['revisions']
+    revs = local_cache[query.pattern()]["revisions"]
     out = []
     for rev_id in revs.keys():
         packages = revs[rev_id]["packages"]
@@ -40,17 +40,15 @@ def parse_packages_json(query: str, json_text: str) -> list[PackageInfo]:
             settings = info.get("settings", {})
             options = info.get("options", {})
             out.append(
-                PackageInfo(
-                    query.name, query.version, rev_id, package_id, settings, options
-                )
+                PackageInfo(query.name, query.version, rev_id, package_id, settings, options)
             )
     return out
 
 
 def list_packages(query: PackageQuery) -> list[PackageInfo]:
-    command = ['conan', 'list', '-c', '-f', 'json', f"{query.pattern()}:*"]
+    command = ["conan", "list", "-c", "-f", "json", f"{query.pattern()}:*"]
     if query.query:
-        command += ['-p', query.query]
+        command += ["-p", query.query]
     # print("List command: " + str(command))
 
     result = subprocess.run(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -61,9 +59,9 @@ def list_packages(query: PackageQuery) -> list[PackageInfo]:
 
 
 def download_packages(query: PackageQuery) -> list[PackageInfo]:
-    command = ['conan', 'download', '-r', 'conancenter', query.pattern(), '-f', 'json']
+    command = ["conan", "download", "-r", "conancenter", query.pattern(), "-f", "json"]
     if query.query:
-        command += ['-p', query.query]
+        command += ["-p", query.query]
     # print("Download command: " + str(command))
 
     result = subprocess.run(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -84,7 +82,7 @@ def get_best_package(packages: list[PackageInfo]) -> PackageInfo:
 
 
 def get_package_path(info: PackageInfo):
-    command = ['conan', 'cache', 'path', info.pattern()]
+    command = ["conan", "cache", "path", info.pattern()]
     result = subprocess.run(command, stdout=subprocess.PIPE)
     if result.returncode != 0:
         print(f"Error while retrieving package path for: {info.pattern()}")
@@ -96,8 +94,8 @@ def copy_subdirs(package_name: str, dst_dir: str, src_dir: str, subdirs: list[st
     for subdir in subdirs:
         dst_subdir = os.path.join(dst_dir, subdir)
         src_subdir = os.path.join(src_dir, subdir)
-        if package_name == 'freetype' and subdir == 'include':
-            src_subdir = os.path.join(src_subdir, 'freetype2')
+        if package_name == "freetype" and subdir == "include":
+            src_subdir = os.path.join(src_subdir, "freetype2")
         os.makedirs(dst_subdir, exist_ok=True)
         if os.path.isdir(src_subdir):
             shutil.copytree(src_subdir, dst_subdir, dirs_exist_ok=True)
@@ -122,20 +120,18 @@ package_queries = [
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='install_deps',
-        description='Installs all the necessary dependencies for building libfwk on Windows',
+        prog="install_deps",
+        description="Installs all the necessary dependencies for building libfwk on Windows",
     )
-    parser.add_argument('install_path')
+    parser.add_argument("install_path")
     args = parser.parse_args()
 
     if os.name != "nt":
-        print(
-            "Currently the only purpose of install_deps.py is to install dependencies on Windows"
-        )
+        print("Currently the only purpose of install_deps.py is to install dependencies on Windows")
         exit(1)
 
     install_path = args.install_path
-    install_path = os.path.join(install_path, 'x86_64')
+    install_path = os.path.join(install_path, "x86_64")
     print(f"Installing packages at: {install_path}")
     os.makedirs(install_path, exist_ok=True)
 
@@ -153,9 +149,7 @@ def main():
         package_path = get_package_path(package)
 
         print(f"Installing {package.pattern()} from: {package_path}")
-        copy_subdirs(
-            package.name, install_path, package_path, ['include', 'lib', 'bin']
-        )
+        copy_subdirs(package.name, install_path, package_path, ["include", "lib", "bin"])
 
 
 if __name__ == "__main__":
