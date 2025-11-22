621faefe367ee39d0d6e280de7f1b846fc404153
Krzysztof Jakubowski <nadult@fastmail.fm>
2025-11-22 10:38:02 +0100

    Cleaned-up source file lists in CMake file, added headers (#92)

--- src/gui/imgui_demo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (C) Krzysztof Jakubowski <nadult@fastmail.fm>
-// This file is part of libfwk. See license.txt for details.
-
-#ifndef FWK_IMGUI_DEMO_DISABLED
-#include "../extern/imgui/imgui_demo.cpp"
-#endif
--- src/gui/imgui_tables.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright (C) Krzysztof Jakubowski <nadult@fastmail.fm>
-// This file is part of libfwk. See license.txt for details.
-
-#include "../extern/imgui/imgui_tables.cpp"
